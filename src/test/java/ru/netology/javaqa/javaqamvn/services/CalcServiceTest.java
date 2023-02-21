dd0ea2e12e82e8d508885532fee1bcd084aa7c04
Alex <zazerny@gmail.com>
2023-02-21 12:58:37 +0300

    Task CsvFileSource

--- src/test/java/ru/netology/javaqa/javaqamvn/services/CalcServiceTest.java
+++ src/test/java/ru/netology/javaqa/javaqamvn/services/CalcServiceTest.java
@@ -3,14 +3,16 @@ package ru.netology.javaqa.javaqamvn.services;
 import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.CsvFileSource;
 import org.junit.jupiter.params.provider.CsvSource;
 
 public class CalcServiceTest {
     @ParameterizedTest
-    @CsvSource({
-            "100000, 60000, 150000, 2",
-            "10000, 3000, 20000, 3"
-    })
+//    @CsvSource({
+//            "100000, 60000, 150000, 2",
+//            "10000, 3000, 20000, 3"
+//    })
+    @CsvFileSource(files = "src/test/resources/calc.csv")
 
     public void testCalculateDifferenceLimit(int income, int expenses, int threshold, int expected) {
         CalcService service = new CalcService();
